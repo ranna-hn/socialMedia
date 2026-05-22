32a3f5eab5ac1ce8401893d86e0addd61ba1df03
rania <aaa@gmail.com>
2026-05-22 22:51:17 +0100

    ajouter ckEditor

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -112,11 +112,11 @@ function deletePost() {
 
             <div class="mb-3">
                 <Disclosure v-slot="{ open }">
-                <div v-if="!open"  v-html="post.body.substring(0, 200)" />
+                <div class="ck-content-output" v-if="!open"  v-html="post.body.substring(0, 200)" />
 
                 <template v-if="post.body.length > 200 ">
                     <DisclosurePanel>
-                    <div v-html="post.body" />
+                    <div class="ck-content-output" v-html="post.body" />
                 </DisclosurePanel>
                     <div class="flex justify-end">
                         <DisclosureButton class="mt-2 text-sm text-blue-500 hover:underline">
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -42,7 +42,8 @@
                 </DialogTitle>
                 <div class="p-4">
                     <PostUserHeader :post="post" :show-time="false" class="mb-4"/>
-                    <InputTextarea v-model="form.body" class="mb-3 w-full" />
+                    <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
+                    <!-- <InputTextarea v-model="form.body" class="mb-3 w-full" /> -->
                 </div>
 
                 <div class="py-3 px-4 ">
@@ -73,10 +74,36 @@ import {
   DialogPanel,
   DialogTitle,
 } from '@headlessui/vue'
-import InputTextarea from '../InputTextarea.vue'
 import PostUserHeader from './PostUserHeader.vue'
 import {XMarkIcon} from '@heroicons/vue/24/solid'
 import { useForm } from '@inertiajs/vue3'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+
+const editor = ClassicEditor;
+
+const editorConfig = {
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            'bold',
+            'italic',
+            'link',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'undo',
+            'redo',
+            '|',
+            'outdent',
+            'indent',
+            '|',
+            'blockQuote'
+        ]
+    }
+    
+};
 
 const props = defineProps({
     post: {
